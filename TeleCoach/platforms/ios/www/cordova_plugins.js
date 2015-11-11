5091435109257e75b0012c79b08333dd5a5add1b
robinrogne <robinrogne@nod085.eduroam.kau.se>
2015-11-11 08:59:56 +0100

    globalization

--- TeleCoach/platforms/ios/www/cordova_plugins.js
+++ TeleCoach/platforms/ios/www/cordova_plugins.js
@@ -1,5 +1,22 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-globalization/www/GlobalizationError.js",
+        "id": "cordova-plugin-globalization.GlobalizationError",
+        "pluginId": "cordova-plugin-globalization",
+        "clobbers": [
+            "window.GlobalizationError"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-globalization/www/globalization.js",
+        "id": "cordova-plugin-globalization.globalization",
+        "pluginId": "cordova-plugin-globalization",
+        "clobbers": [
+            "navigator.globalization"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {}
