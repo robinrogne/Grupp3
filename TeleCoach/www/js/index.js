ee227c366f1d8ee2f9aa6358e1fcf16338e28f6c
Deepeeper <albin.pettersson@hotmail.com>
2015-11-11 11:13:37 +0100

    Lite javascript och caards.html
    Även skapat en textfil i mappen texts

--- TeleCoach/www/js/index.js
+++ TeleCoach/www/js/index.js
@@ -4,3 +4,30 @@ function leader(){
 function coworker(){
 	window.location="coworker.html";
 }
+function loadcards(){
+    var traningskort1 = "Det här är texten för träningskort1";
+    $("p").text(traningskort1);
+}
+//function readTextFile(file)
+//{
+//    alert("readTExtfile");
+//    var rawFile = new XMLHttpRequest();
+//    rawFile.open("GET", file, false);
+//    rawFile.onreadystatechange = function ()
+//    {
+//        if(rawFile.readyState === 4)
+//        {
+//            if(rawFile.status === 200 || rawFile.status == 0)
+//            {
+//                var allText = rawFile.responseText;
+//                alert(allText);
+//                //loadcards(allText);
+//            }
+//        }
+//    }
+//    rawFile.send(null);
+//}
+function hej(){
+
+}
+
